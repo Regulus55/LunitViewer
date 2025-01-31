12ef9bd81f27d96998ee461d377187adf11b8ede
Regulus55 <hakjoon55@gmail.com>
2025-01-31 19:59:33 +0900

    image show done

--- src/pages/Contour/Viewer.js
+++ src/pages/Contour/Viewer.js
@@ -1,26 +1,38 @@
-import React, { useState, useEffect } from 'react';
-import { Img } from 'react-image';
+import React from 'react'
+import InsightViewer, { useMultipleImages, useFrame } from '@lunit/insight-viewer'
 
 const IMAGES = [
-  '/images/2.16.840.1.114362.1.11890052.23347336132.553945887.744.1174.dcm',
-  '/images/2.16.840.1.114362.1.11890052.23347336132.553945887.746.1175',
-  '/images/2.16.840.1.114362.1.11890052.23347336132.553945887.749.1176',
-  '/images/2.16.840.1.114362.1.11890052.23347336132.553945887.755.1178',
-  '/images/2.16.840.1.114362.1.11890052.23347336132.553945887.757.1179',
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000000.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000001.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000002.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000003.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000004.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000005.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000006.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000007.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000008.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000009.dcm",
+  "wadouri:https://static.lunit.io/fixtures/dcm-files/series/CT000010.dcm"
 ];
-
 
 const style = {
   width: '500px',
-  height: '500px',
-};
+  height: '500px'
+}
 
 export default function Viewer() {
-  const [currentFrame, setCurrentFrame] = useState(0);
+  const { images } = useMultipleImages({
+    wadouri: IMAGES
+  })
+
+  const { frame, setFrame } = useFrame({
+    initial: 0,
+    max: images.length - 1,
+  })
 
   function changeFrame(e) {
-    const { value } = e.target;
-    setCurrentFrame(Number(value));
+    const { value } = e.target
+    setFrame(Number(value))
   }
 
   return (
@@ -34,11 +46,11 @@ export default function Viewer() {
         step="1"
         onChange={changeFrame}
         className="frame-control"
-        value={currentFrame}
+        value={frame}
       />
       <div style={style}>
-        <Img src={IMAGES[currentFrame]} alt="Dicom Image" />
+        <InsightViewer image={images[frame]} />
       </div>
     </div>
-  );
+  )
 }
